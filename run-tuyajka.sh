6ceb5a2feaa180dcc43dbee6cfe4dabe91de663a
0101binary0101 <jka@twiddlingthumbs.com>
2020-12-08 19:14:25 +0000

    resolve silly config error in directory checking

--- run-tuyajka.sh
+++ run-tuyajka.sh
@@ -1,11 +1,11 @@
 
 # 
-tuyashare="${1:-"/usr/share/hassio/share/tuyapower/"}"
-echo "change your the tuyashare to be something like /usr/share/hassio/share/tuyapower/ if running on HA /usr/share/hassio/share/tuyapower/ is probably accessible outside of HA docker" 
-if [ ! -d "${1} ]; then
-	echo "Maybe you'd like to create the /usr/share/hassio/share/tuyapower/
+tuyashare=${1:-"/usr/share/hassio/share/tuyapower/"}
+if [ ! -d "${tuyashare}" ]; then
+	echo "change your the tuyashare to be something like /usr/share/hassio/share/tuyapower/ if running on HA /usr/share/hassio/share/tuyapower/ is probably accessible outside of HA docker" 
+	echo "Maybe you'd like to create the /usr/share/hassio/share/tuyapower/ or specify a different directory under /usr/share/hassio/share/"
 	exit 1
 fi
 
 
-docker run -d --restart=unless-stopped --network=host --name tuyajka --volume=${tuyashare}:/share-tuya/:rw tuyajka
+docker run -d --restart=unless-stopped --network=host --name tuyajka --volume=${tuyashare}/:/share-tuya/:rw tuyajka
