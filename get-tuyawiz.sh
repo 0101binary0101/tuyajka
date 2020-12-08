c046216d11e713890fb2ae78f8a9733e53f0f314
0101binary0101 <jka@twiddlingthumbs.com>
2020-12-08 01:52:34 +0000

    resolve config details

--- get-tuyawiz.sh
+++ get-tuyawiz.sh
@@ -2,6 +2,8 @@
 echo .
 echo "You will need your tuya API key and API secret"
 echo .
+# put the user api details in the share area, it should be protected enough from access
+export HOME=/share-tuya/
 tuya-cli wizard
 echo "# now copy the above JSON output and place this into your /share/tuyapower/tuya-wizard.out file"
 echo "# once you had done this you can create the index.plug file - docker exec -ti tuyajka bash /local-tuya/create-tuya-index.sh"
