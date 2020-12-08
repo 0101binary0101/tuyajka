b3d65fa8cc6042febeeda1b45de1855a27ed751b
0101binary0101 <jka@twiddlingthumbs.com>
2020-12-08 01:52:17 +0000

    resolve config details

--- run-tuyajka.sh
+++ run-tuyajka.sh
@@ -1,6 +1,11 @@
 
 # 
-echo "change your the tuyashare to be something like /share/tuyapower/ if running on HA /share is probably accessible outside of HA docker" 
-tuyashare="${1:-"/share/tuyapower/"}"
+tuyashare="${1:-"/usr/share/hassio/share/tuyapower/"}"
+echo "change your the tuyashare to be something like /usr/share/hassio/share/tuyapower/ if running on HA /usr/share/hassio/share/tuyapower/ is probably accessible outside of HA docker" 
+if [ ! -d "${1} ]; then
+	echo "Maybe you'd like to create the /usr/share/hassio/share/tuyapower/
+	exit 1
+fi
+
 
 docker run -d --restart=unless-stopped --network=host --name tuyajka --volume=${tuyashare}:/share-tuya/:rw tuyajka
